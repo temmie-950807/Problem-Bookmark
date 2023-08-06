c78adfd4ea34d576f073b27674ed2c04673b4011
kouke0638 <pan6083a@gmail.com>
2023-08-06 13:07:40 +0800

    feat: + 添加非ATC題目提醒
    + 新增註解

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// popup.html 的 JS 檔案
 // getting all required elements
 const searchInput = document.querySelector("#search-input");
 const input = searchInput.querySelector("#search-bar");
@@ -7,18 +8,29 @@ const saveButton = document.querySelector("#save-button");
 const Storage = chrome.storage.local;
 
 
-chrome.tabs.query({'active' : true, 'currentWindow' : true}, (e) => {
-    let url = e[0].url;
-    if (!url) {
+chrome.tabs.query({ 'active': true, 'currentWindow': true }, (tabs) => {
+    if (!tabs || tabs.length === 0 || !tabs[0].url) {
         window.close();
+        return;
     }
+    let url = tabs[0].url;
     const regex = [
-        /^https:\/\/atcoder\.jp\/contests\/.+?\/tasks\/.+$/, 
+        /^https:\/\/atcoder\.jp\/contests\/.+?\/tasks\/.+$/,
         /^$/
     ];
-    if (!regex.some(a => a.test(url))){
-        // 確保是 ATC 的題目
-        window.close();
+    if (!regex.some(a => a.test(url))) {
+        // 確保是 ATC 的題目頁面
+        // 發出通知chrome.notifications
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'assets/icon.png',
+            title: 'Judge Bookmark',
+            message: 'This page is not a ATC problem page.',
+        });
+        // 修改popup.html的內容，將body改成錯誤訊息
+        document.body.innerHTML = '<h1 class="errorPage">This page is not a ATC problem page.</h1>';
+        // window.close();
+        return;
     }
     Storage.get(["problems"]).then((result) => {
         /*
@@ -35,6 +47,8 @@ chrome.tabs.query({'active' : true, 'currentWindow' : true}, (e) => {
             showTags(result.problems[url]);
         input.addEventListener('keyup', (event) => inputKeyup(event, result.problems));
         saveButton.addEventListener('click', (event) => save(event, url, result.problems));
+    }).catch((error) => { // 如果發生錯誤
+        console.error("Error while fetching from storage:", error);
     });
 });
 
@@ -66,9 +80,9 @@ function inputKeyup(event, problems) {
 
 // 處理搜尋清單
 function lcs(text1, text2) {
-    const result = new Array(text1.length + 1).fill(null).map(() => new Array(text2.length + 1).fill(null))
+    const result = new Array(text1.length + 1).fill(null).map(() => new Array(text2.length + 1).fill(null)) // 建立一個二維陣列
 
-    function test(end1, end2) {
+    function test(end1, end2) { // 遞迴函式
         if (end1 === -1 || end2 === -1) {
             return 0
         }
@@ -89,12 +103,14 @@ function lcs(text1, text2) {
     return test(text1.length - 1, text2.length - 1)
 }
 
+// 處理使用者選擇
 function select(event) {
-    input.value = "";
-    searchInput.classList.remove("active");
-    addTag(event.target.dataset.value);
+    input.value = ""; // 清空搜尋欄
+    searchInput.classList.remove("active"); // 隱藏搜尋清單
+    addTag(event.target.dataset.value); // 新增 Tag
 }
 
+// 處理搜尋清單
 function showSuggestions(suggestions, userValue) {
     resultBox.innerHTML = "";
     suggestions.forEach(tag => {
@@ -118,6 +134,7 @@ function showTags(tags) {
     tags.forEach(addTag);
 }
 
+// 新增 Tag
 function addTag(tag) {
     let tags = [...currentTags.querySelectorAll('.tag')].map((elem) => elem.innerHTML);
     if (tags.includes(tag)) return;
@@ -131,9 +148,12 @@ function addTag(tag) {
     currentTags.appendChild(tagHTML);
 }
 
+
+// 儲存
 function save(event, url, problems) {
     let tags = [...currentTags.querySelectorAll('.tag')].map((elem) => elem.innerHTML);
     problems[url] = tags;
+    // 儲存到 chrome.storage
     Storage.set({ "problems" : problems }).then(() => {
         console.log(`The tags of ${url} is now ${tags}`);
         window.close();

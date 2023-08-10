e0bc64a9a1a2b061a971be4194f6782a6821f407
temmie_950807 <kapoo950807@gmail.com>
2023-08-10 18:31:55 +0800

    優化渲染函式

--- query/query.js
+++ query/query.js
@@ -1,152 +1,56 @@
-// popup.html 的 JS 檔案
 // getting all required elements
-const searchInput = document.querySelector("#search-input");
-const input = searchInput.querySelector("#search-bar");
-const resultBox = searchInput.querySelector("#result-box");
-const currentTags = document.querySelector("#current-tags");
 const Storage = chrome.storage.local;
 
 function renderProblemList(problems) {
     const problemList = document.querySelector("#problem tbody");
+
+    // 清空題目渲染區
     problemList.innerHTML = "";
-    console.log(Object.keys(problems));
+    
+    // 取得每一筆題目的資料，並且渲染上去
     for (const url of Object.keys(problems)) {
         let {name, difficulty, tags, comment} = problems[url];
-        const rowHTML = `<tr><td><a href=${url}>${name}</a></td><td>${difficulty}</td><td>${tags}</td><td>${comment}</td></tr>`;
+
+        // 將難度表轉換成星星
+        difficulty = parseInt(difficulty);
+        let difficulty_star = "<i class=\"fas fa-star\"></i>".repeat(difficulty)+"<i class=\"far fa-star\"></i>".repeat(5-difficulty);
+
+        // 將 tags 拆開成 <p>
+        console.log(tags);
+        let tag_element = "";
+        for (x of tags){
+            tag_element += `<p class="tag">${x}</p>`;
+        }
+
+        const rowHTML = `
+        <tr>
+            <td><a href=${url}>${name}</a></td>
+            <td>${difficulty_star}</td>
+            <td>${tag_element}</td>
+            <td>${comment}</td>
+        </tr>`;
         problemList.innerHTML += rowHTML;
     }
 }
 
-chrome.tabs.query({ 'active': true, 'currentWindow': true }, (tabs) => {
-    if (!tabs || tabs.length === 0 || !tabs[0].url) {
-        window.close();
-        return;
-    }
-    const url = tabs[0].url;
-    Storage.get(["problems"]).then((result) => {
-        
-        // template for query result
-        const resultTemplate = {
-            "problems": {
-                "url": {
-                    name: "name",
-                    difficulty: "Easy",
-                    tags: ["tag1", "tag2"],
-                    note: "note"
-                }
+// 讀取題目資料
+Storage.get(["problems"]).then((result) => {
+    
+    // template for query result
+    const resultTemplate = {
+        "problems": {
+            "url": {
+                name: "name",
+                difficulty: "Easy",
+                tags: ["tag1", "tag2"],
+                note: "note"
             }
         }
-        
-        if (!result.problems)
-            result.problems = {};
-        if (result.problems.hasOwnProperty(url))
-            showTags(result.problems[url]);
-        input.addEventListener('keyup', (event) => inputKeyup(event, result.problems));
-        renderProblemList(result.problems);
-    });
-});
-
-// 如果使用者按下任何按鍵就要觸發這個函式
-function inputKeyup(event, problems) {
-    let tags = [...new Set(Object.values(problems).flat())].sort(); // 把所有 problem 的 tag 直接取出
-    let userValue = event.target.value; // 使用者輸入的文字
-    if (userValue) {
-        // 如果使用者按下 Enter，直接匹配第一個搜尋結果
-        if (event.code == "Enter") {
-            resultBox.querySelector("li").dispatchEvent(new Event('click'));
-            return;
-        }
-        // 用 LCS 判斷 tag 的優先度
-        let suggestions = tags.map((data) => [lcs(data.toLocaleLowerCase(), userValue.toLocaleLowerCase()), data])
-                              .sort()
-                              .reverse()
-                              .slice(0, 7)
-                              .map((data) => data[1]);
-
-        searchInput.classList.add("active"); //show autocomplete box
-        showSuggestions(suggestions, userValue);
-    } else {
-        searchInput.classList.remove("active"); //hide autocomplete box
     }
-}
-
-// 處理搜尋清單
-function lcs(text1, text2) {
-    const result = new Array(text1.length + 1).fill(null).map(() => new Array(text2.length + 1).fill(null)) // 建立一個二維陣列
-
-    function test(end1, end2) { // 遞迴函式
-        if (end1 === -1 || end2 === -1) {
-            return 0
-        }
-        if (result[end1][end2] !== null) {
-            return result[end1][end2]
-        }
-        if (text1[end1] === text2[end2]) {
-            result[end1][end2] = 1 + test(end1 - 1, end2 - 1)
-            return result[end1][end2]
-        } else {
-            result[end1][end2] = Math.max(
-                test(end1 - 1, end2),
-                test(end1, end2 - 1)
-            )
-            return result[end1][end2]
-        }
-    }
-    return test(text1.length - 1, text2.length - 1)
-}
-
-// 處理使用者選擇
-function select(event) {
-    input.value = ""; // 清空搜尋欄
-    searchInput.classList.remove("active"); // 隱藏搜尋清單
-    addTag(event.target.dataset.value); // 新增 Tag
-}
-
-// 處理搜尋清單
-function showSuggestions(suggestions, userValue) {
-    resultBox.innerHTML = "";
-    suggestions.forEach(tag => {
-        let suggestion = document.createElement('li');
-        suggestion.innerHTML = tag;
-        suggestion.dataset.value = tag;
-        suggestion.addEventListener('click', select);
-        resultBox.appendChild(suggestion);
-    });
-    if (!suggestions.includes(userValue)) {
-        let newTag = document.createElement('li');
-        newTag.innerHTML = `Add "${userValue}"`;
-        newTag.dataset.value = userValue;
-        newTag.addEventListener('click', select);
-        resultBox.appendChild(newTag);
-    }
-}
-
-// 處理 Tag 系統
-function showTags(tags) {
-    tags.forEach(addTag);
-}
-
-// 新增 Tag
-function addTag(tag) {
-    let tags = [...currentTags.querySelectorAll('.tag')].map((elem) => elem.innerHTML);
-    if (tags.includes(tag)) return;
     
-    let tagHTML = document.createElement('span');
-    tagHTML.innerHTML = tag;
-    tagHTML.classList.add('tag');
-    tagHTML.addEventListener('click', (event) => {
-        currentTags.removeChild(event.target);
-    });
-    currentTags.appendChild(tagHTML);
-}
+    // 如果沒有資料就丟空字典
+    if (!result.problems)
+        result.problems = {};
 
-// 儲存
-function save(event, url, problems) {
-    let tags = [...currentTags.querySelectorAll('.tag')].map((elem) => elem.innerHTML);
-    problems[url]['tags'] = tags;
-    // 儲存到 chrome.storage
-    Storage.set({ "problems" : problems }).then(() => {
-        console.log(`The tags of ${url} is now ${tags}`);
-        window.close();
-    });
-}
+    renderProblemList(result.problems);
+});

dc8cd325210a307b8ab7932e54fdf6474664cc48
temmie_950807 <kapoo950807@gmail.com>
2023-08-13 18:40:08 +0800

    增加篩選系統

--- query/query.js
+++ query/query.js
@@ -1,5 +1,7 @@
 // getting all required elements
 const Storage = chrome.storage.local;
+const currentTags = document.querySelector("#current-tags");
+const orBox = document.querySelector("#or-box");
 
 function renderProblemList(problems) {
     const problemList = document.querySelector("#problem tbody");
@@ -8,17 +10,23 @@ function renderProblemList(problems) {
     problemList.innerHTML = "";
     
     // 取得每一筆題目的資料，並且渲染上去
+    const or = orBox.checked;
+    const queryTags = [...currentTags.querySelectorAll(".tag")].map((elem) => elem.value);
+
     for (const url of Object.keys(problems)) {
         let {name, difficulty, tags, comment} = problems[url];
+        let tagCount = 0;
 
         // 將難度表轉換成星星
         difficulty = parseInt(difficulty);
         let difficulty_star = "<i class=\"fas fa-star\"></i>".repeat(difficulty)+"<i class=\"far fa-star\"></i>".repeat(5-difficulty);
-
+        
         // 將 tags 拆開成 <p>
-        console.log(tags);
         let tag_element = "";
-        for (x of tags){
+        for (x of tags) {
+            if (queryTags.includes(x)) {
+                tagCount += 1;
+            }
             tag_element += `<p class="tag">${x}</p>`;
         }
 
@@ -29,7 +37,12 @@ function renderProblemList(problems) {
             <td>${tag_element}</td>
             <td>${comment}</td>
         </tr>`;
-        problemList.innerHTML += rowHTML;
+
+        if (or === true && tagCount > 0) {
+            problemList.innerHTML += rowHTML;
+        } else if (or === false && tagCount === queryTags.length()) {
+            problemList.innerHTML += rowHTML;
+        }
     }
 }
 
